2ee888265b3208365cc0634685ab00b8ebeb085c
Josue Garcia <yoshuah809@gmail.com>
2021-12-03 08:41:41 -0600

    Created the Vehicle type module

--- drf_jwt_capstone_backend/vehicle/models.py
+++ drf_jwt_capstone_backend/vehicle/models.py
@@ -4,24 +4,18 @@ from django.db.models.fields import CharField, DateField
 
 
 # Create your Models here.
-type_choices = (
+class Vehicle_Type(models.Model):
+    vehicle_type=models.CharField(max_length=15)
 
-    ('Sedan', 'Sedan'),
-    ('Coupe','Coupe'),
-    ('Truck','Truck'),
-    ('SUV', 'SUV'),
-    ('Bus','Bus'),
-    ('Collection', 'Collection'),
-)
 number_of_doors_choices = (   
-    ('2', '2'),
-    ('4','4'),  
-    ('6','6'),
+    (2,2),
+    (4,4),  
+    (6,6),
 )
 rowseat_choices = (   
-    ('1','1'),
-    ('2','2'),
-    ('3','3'),
+    (1,1),
+    (2,2),
+    (3,3),
 )
 fueltype_choices =(
     ('Gas','Gas'),
@@ -40,11 +34,14 @@ class Vehicle(models.Model):
     model = models.CharField(max_length=30)
     mfr = models.IntegerField()
     color = models.CharField(max_length=20)
-    type = models.CharField(max_length=15, choices=type_choices)
+    type = models.ForeignKey(Vehicle_Type, default=None, on_delete=models.PROTECT)
     purchased_date = DateField()
-    date_sold = DateField()
-    fuel_type = models.CharField(max_length=15, choices=fueltype_choices)
+    date_sold = DateField(blank=True, null=True)
+    fuel_type = models.CharField(max_length=15, choices=fueltype_choices, default='Gas')
     features = models.CharField(max_length=100)
-    number_of_doors = models.CharField(max_length=15, choices=number_of_doors_choices)
-    rowseat = models.IntegerField(choices=rowseat_choices)
-    transmission = models.CharField(max_length= 20, choices=transmission_type_choices)
+    number_of_doors = models.IntegerField(choices=number_of_doors_choices, default=4)
+    rowseat = models.IntegerField(choices=rowseat_choices, default=2)
+    transmission = models.CharField(max_length= 20, choices=transmission_type_choices, default='Automatic')
+
+    def __str__(self):
+       return f"{self.make} {self.model} {self.mfr}"
