605f1a837dfdfc8a66737702d4c040b82eb63352
Josue Garcia <yoshuah809@gmail.com>
2021-12-09 14:04:43 -0600

    modified the jwt response to return email and username

--- drf_jwt_capstone_backend/authentication/views.py
+++ drf_jwt_capstone_backend/authentication/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from .serializers import RegistrationSerializer
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 User = get_user_model()
 
 
@@ -9,3 +11,17 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegistrationSerializer
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        data['username'] = self.user.username
+        data['email'] = self.user.email
+
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
--- drf_jwt_capstone_backend/vehicle/serializers.py
+++ drf_jwt_capstone_backend/vehicle/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Vehicle
+from django.contrib.auth import get_user_model
+
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
         fields = '__all__'
+
+
+ 
